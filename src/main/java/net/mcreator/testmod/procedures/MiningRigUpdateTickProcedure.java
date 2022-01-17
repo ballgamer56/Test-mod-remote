526e884123d534efa5d04296b4fdd263c66780eb
ballgamer56 <balltdmfanclub@gmail.com>
2022-01-17 18:24:03 +0700

    MCreator Merge commit '7de3ba09eb2974c90c8bf98936b489160cd49871' into 985e0e40fde3f4b5ad48152934dbc28ca29b9946

--- src/main/java/net/mcreator/testmod/procedures/MiningRigUpdateTickProcedure.java
+++ src/main/java/net/mcreator/testmod/procedures/MiningRigUpdateTickProcedure.java
@@ -1,19 +1,6 @@
 package net.mcreator.testmod.procedures;
 
-import net.minecraftforge.items.IItemHandlerModifiable;
-import net.minecraftforge.items.CapabilityItemHandler;
-
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.core.BlockPos;
-
-import net.mcreator.testmod.init.TestModModItems;
-
-import java.util.concurrent.atomic.AtomicReference;
-import java.util.concurrent.atomic.AtomicInteger;
+import net.minecraftforge.eventbus.api.Event;
 
 public class MiningRigUpdateTickProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z) {
